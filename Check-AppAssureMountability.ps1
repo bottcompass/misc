17d2624294e6d2195753d6d4f8571c63ffbb9c05
Brian Ott <bott@compassconsult.com>
2017-02-09 08:49:06 -0800

    new example files

---
+++ Check-AppAssureMountability.ps1
@@ -0,0 +1,35 @@
+﻿#Requires -Modules RapidRecoveryPowerShellModule
+<#
+.help [string]protectedserver
+The protected server(s) to run the mountability check on.
+#>
+function Check-AppAssureMountability {
+[cmdletbinding()]
+param(
+    [Parameter(Mandatory=$True,
+    ValueFromPipeline=$True)]
+    [string[]]$protectedserver,
+    [string]$core="localhost"
+    )
+    BEGIN {
+
+    }
+    PROCESS {
+            ForEach ( $server in $protectedserver )
+            {
+                $jobName = "Job-$server"
+                $jobScriptBlock = { 
+                                    param ( $core,$server ) 
+                                    Start-MountabilityCheck -Core $core -ProtectedServer $server
+                                  }
+                $newJob = Start-Job -Name $jobName -ScriptBlock (& $jobScriptBlock -core $core -server $server)
+
+                Wait-Job -Job $newJob
+
+                # checking if job completed successfully in the core logs
+
+
+            }
+    }
+    END {}
+}
---
+++ Examples-WindowsUpdate.ps1
@@ -0,0 +1,15 @@
+﻿$servers = get-adcomputer -filter 'OperatingSystem -like "*server*"' | select Name
+
+foreach ( $server in $servers )
+{
+    $serverName = $server.Name
+    if ( test-connection -computername $serverName -quiet -delay 1 -count 1 )
+    {
+
+        Start-Job -Name "Job-$serverName" -ArgumentList $serverName -ScriptBlock { 
+        get-windowsupdate -computername $args[0] -verbose
+        }
+
+    }
+}
+
